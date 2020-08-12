59e013aa0893974aaacdd5f4bbafb28f64ad3253
J.Yan <yaoyaoshang@126.com>
2020-08-11 20:34:36 -0700

    added some new functions and fixed some bugs: 1,add new functions that read in frames instead of videos 2,extracted some code snippets to form some independent modules 3,fixed some minor bugs

--- VGG-face+BiLSTM.py
+++ VGG-face+BiLSTM.py
@@ -23,10 +23,10 @@ import matplotlib.pyplot as plt
 # a function that creates a CNN model
 def create_cnn():
     # create the pre-trained VGG-Face model
-    cnn_model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3))
+    cnn_model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), weights=None)
 
     # load the weights from the pre-trained model
-    # cnn_model.load_weights('rcmalli_vggface_tf_notop_resnet50.h5')
+    cnn_model.load_weights('rcmalli_vggface_tf_notop_resnet50.h5')
 
     # compile the model
     cnn_model.compile(optimizer=SGD(), loss='categorical_crossentropy', metrics=['accuracy'])
@@ -51,11 +51,62 @@ def create_bi_lstm(n_frames, n_tokens):
     return lstm_model
 
 
-# a function that extracts features from a given smile video
-def extract_features(vpath, cnn_model):
-    # get face detector from dlib and initialize the face aligner
+# a function that extracts features from a given frame
+def extract_features_frame(frame, cnn_model):
+    # get face detector from dlib
     detector = dlib.get_frontal_face_detector()
 
+    # convert to grayscale
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect a valid face from the frame
+    faces = detector(frame_gray, 1)
+
+    # return None if not exactly one face is detected
+    if len(faces) != 1:
+        return None
+
+    # get the coordinates of the bounding box
+    (x, y, w, h) = rect_to_bb(faces[0])
+
+    # crop the negative values
+    if x < 0:
+        w = w + x
+        x = 0
+    if y < 0:
+        h = h + y
+        y = 0
+
+    # resize the image to 224x224
+    # face = imutils.resize(frame[x:x + w, y:y + h], width=224, height=224)
+    try:
+        face = cv2.resize(frame[x:x + w, y:y + h], (224, 224), interpolation=cv2.INTER_AREA)
+    except Exception as e:
+        print(e)
+        print(f'x={x},y={y},w={w},h={h}')
+        print(f'The shape of the face is {frame[x:x + w, y:y + h].shape}')
+        print(f'The shape of the frame is {frame_gray.shape}')
+        # skip this frame and return None
+        return None
+
+    # expand the shape of the image by one dimension
+    face_in = np.expand_dims(face, axis=0)
+
+    # convert to dtype 'float64'
+    face_in = face_in.astype('float64')
+
+    # some preprocessing required by the model
+    face_in = preprocess_input(face_in, version=2)
+
+    # feature extraction using CNN
+    feature = cnn_model.predict(face_in).ravel()
+
+    # return the extracted feature
+    return feature
+
+
+# a function that extracts features from a given smile video
+def extract_features(vpath, cnn_model):
     # open the video file
     video_cap = cv2.VideoCapture(vpath)
 
@@ -73,56 +124,22 @@ def extract_features(vpath, cnn_model):
 
         # process this frame if successful
         if success:
-            # convert to grayscale
-            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # detect a valid face from the frame
-            faces = detector(frame_gray, 2)
+            # extract features from the frame
+            feature = extract_features_frame(frame, cnn_model)
 
-            # continue if more than one face is detected
-            if len(faces) != 1:
-                continue
+            # add the extracted features to the list
+            # if not None
+            if feature is not None:
+                features.append(feature)
 
-            # get the coordinates of the bounding box
-            (x, y, w, h) = rect_to_bb(faces[0])
-
-            # crop the negative values
-            if x < 0:
-                w = w + x
-                x = 0
-            if y < 0:
-                h = h + y
-                y = 0
-
-            # resize the image to 224x224
-            # face = imutils.resize(frame[x:x + w, y:y + h], width=224, height=224)
-            try:
-                face = cv2.resize(frame[x:x + w, y:y + h], (224, 224), interpolation=cv2.INTER_AREA)
-            except Exception as e:
-                print(e)
-                print(f'x={x},y={y},w={w},h={h}')
-                print(f'The shape of the face is {frame[x:x + w, y:y + h].shape}')
-                print(f'The shape of the frame is {frame_gray.shape}')
-                print(f'The file is {vpath}')
-                sys.exit(-1)
-
-            # expand the shape of the image by one dimension
-            face_in = np.expand_dims(face, axis=0)
-
-            # convert to dtype 'float64'
-            face_in = face_in.astype('float64')
-
-            # some preprocessing required by the model
-            face_in = preprocess_input(face_in, version=2)
-
-            # feature extraction using CNN
-            feature = cnn_model.predict(face_in).ravel()
-
-            # add to the list
-            features.append(feature)
-
-    # convert to ndarray and return the results
-    return np.array(features)
+    # if features is not empty
+    if len(features) > 0:
+        # convert to ndarray and return the results
+        return np.array(features)
+    else:
+        # return None, otherwise
+        return None
 
 
 # a function that extracts features from all videos in a given folder
@@ -147,11 +164,84 @@ def extract_features_dir(vdir, cnn_model):
     for vpath in vpaths:
         print(f'Current File:{vpath}')
         feature = extract_features(vpath, cnn_model)
-        if feature.size != 0:
+        if feature is not None:
             features.append(feature)
 
-    # return the results as a ndarray
-    return np.array(features)
+    # if features is not empty
+    if len(features) > 0:
+        # convert to ndarray and return the results
+        return np.array(features)
+    else:
+        # return None, otherwise
+        return None
+
+
+# a function that extracts features from all the frames in a given folder
+def extract_features_dir_2(fdir, cnn_model):
+    # check the existence of the directory
+    if not os.path.exists(fdir):
+        raise ValueError('The directory "{}" does not exist!'.format(fdir))
+
+    # create an empty list to hold the features
+    # for all videos in the folder
+    features = []
+
+    # list all subdirectories
+    sub_dirs = [sub_dir for sub_dir in os.listdir(fdir)
+                if os.path.isdir(os.path.join(fdir, sub_dir))]
+
+    # get the number part of the file name using the regular expression
+    reg1 = re.compile('_(\d*)')
+
+    # sort the file names
+    sub_dirs.sort(key=lambda x: int(reg1.search(x).group(1)))
+
+    for sub_dir in sub_dirs:
+
+        # create an empty list to store features for each video
+        features_video = []
+
+        # form a path for each sub-folder
+        sub_dir_path = os.path.join(fdir, sub_dir)
+
+        # print the path we are currently processing
+        print(sub_dir_path)
+
+        # get the list of frames in this directory
+        fpaths = glob(os.path.join(sub_dir_path, '*.jpg'))
+
+        # get the number part of the file name using the regular expression
+        reg2 = re.compile('\D*(\d*)[.]jpg')
+
+        # sort the file names
+        fpaths.sort(key=lambda x: int(reg2.search(x).group(1)))
+
+        # extract features from each video
+        for fpath in fpaths:
+
+            # read in the image
+            frame = cv2.imread(fpath)
+
+            # extract features from the frame
+            features_tmp = extract_features_frame(frame, cnn_model)
+
+            # add the extracted features to the list
+            # if not None
+            if features_tmp is not None:
+                features_video.append(features_tmp)
+
+        # aggregate the extracted features of each video
+        # to a single list if not empty
+        if len(features_video) > 0:
+            features.append(np.array(features_video))
+
+    # if the resulting features is not empty
+    if len(features) > 0:
+        # convert to ndarray and return the results
+        return np.array(features)
+    else:
+        # return None, otherwise
+        return None
 
 
 # a function that generates batches
@@ -172,7 +262,16 @@ def generate_batch(x_samples, y_samples, batch_size, expected_frames):
                     x = x[0:expected_frames, :]
                     x_data.append(x)
                 elif frames < expected_frames:
-                    temp = np.zeros(shape=(expected_frames, x.shape[1]))
+                    try:
+                        temp = np.zeros(shape=(expected_frames, x.shape[1]))
+                    except IndexError as e:
+                        print(e)
+                        print(f'The shape of x is = {x.shape}')
+                        print(f'frames = {frames}')
+                        print(f'expected_frames = {expected_frames}')
+                        print(f'batchIdx = {batchIdx}')
+                        print(f'k = {k}')
+                        sys.exit(-1)
                     temp[0:frames, :] = x
                     x_data.append(temp)
                 else:
@@ -195,19 +294,19 @@ def main():
     # check if saved files exist
     if not os.path.exists('dataset.npz'):
         # construct the path for the directory of happy smiles
-        dir_happy = os.path.join('dataset-video', 'Happy')
+        dir_happy = os.path.join('.', 'happy_frames')
 
         # construct the path for the directory of awkward smiles
-        dir_awkward = os.path.join('dataset-video', 'Awkward')
+        dir_awkward = os.path.join('.', 'nervous_frames')
 
         # create a VGG-Face CNN model
         cnn_model = create_cnn()
 
         # get the features for happy smiles
-        features_happy = extract_features_dir(dir_happy, cnn_model)
+        features_happy = extract_features_dir_2(dir_happy, cnn_model)
 
         # get the features for awkward smiles
-        features_awkward = extract_features_dir(dir_awkward, cnn_model)
+        features_awkward = extract_features_dir_2(dir_awkward, cnn_model)
 
         # combine these features
         features = np.concatenate((features_happy, features_awkward), axis=0)
